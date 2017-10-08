4efdfc75a4349f838c8aa8d774f750f17ab233d0
Philip Chang <sgnoohc@gmail.com>
2017-10-07 19:28:38 -0700

    Add files

---
+++ hadd.sh
@@ -0,0 +1,7 @@
+hadd -f outputs/fakerate_qcd_mu.root /hadoop/cms/store/user/phchang/metis/fr/fr_v2/QCD_Pt_*Mu*/*root
+hadd -f outputs/fakerate_qcd_el.root /hadoop/cms/store/user/phchang/metis/fr/fr_v2/QCD_Pt_*EM*/*root /hadoop/cms/store/user/phchang/metis/fr/fr_v2/QCD_Pt_*bcToE*/*root
+hadd -f outputs/fakerate_data.root /hadoop/cms/store/user/phchang/metis/fr/fr_v2/*Double*/*root
+hadd -f outputs/fakerate_wj.root /hadoop/cms/store/user/phchang/metis/fr/fr_v2/*WJ*/*root
+hadd -f outputs/fakerate_dy.root /hadoop/cms/store/user/phchang/metis/fr/fr_v2/*DY*/*root
+hadd -f outputs/fakerate_ttbar.root /hadoop/cms/store/user/phchang/metis/fr/fr_v2/*TTbar*/*root
+hadd -f outputs/fakerate_vv.root /hadoop/cms/store/user/phchang/metis/fr/fr_v2/*WW*/*root /hadoop/cms/store/user/phchang/metis/fr/fr_v2/*WZ*/*root /hadoop/cms/store/user/phchang/metis/fr/fr_v2/*ZZ*/*root
